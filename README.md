feb089820c871b6a5f1a9fdf3a8ea5590cafa3ff
GrandEmetak <JekHamond@tutanota.com>
2023-04-18 21:51:31 +0500

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+### APP Converter Service
+
+Used technologies
+- Kotlin Core
+- Rest API
+- Spring Boot (Data JPA, Web)
+- Maven - Build Tool
+- PostgreSQL (DB)
+- Postman - Test REST API
+- Travis C.I.
---
+++ src/test/kotlin/com/domain/converter/AddressConverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.domain.converter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AddressConverterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
